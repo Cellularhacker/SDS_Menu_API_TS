65be348db6300f069a92ce254aae6745638af426
Jeffrey Lee <jeffrey@wizpace.com>
2019-05-06 03:12:44 +0900

    added debugging point

--- src/model/maria/SDSCorner/SdsCorner.ts
+++ src/model/maria/SDSCorner/SdsCorner.ts
@@ -8,11 +8,17 @@ export default class SDSCorner {
   }
   public ask(id: string): Promise<string> {
     return new Promise((resolve, reject) => {
+      console.log(`Inside of Function`);
+      console.log(`query ===>`, this.client.conn
+          .select(this.client.sdsCorner.CornerName)
+          .from(this.client.sdsCorner._t_name)
+          .where(this.client.sdsCorner.code, id).toSQL());
       this.client.conn
         .select(this.client.sdsCorner.CornerName)
         .from(this.client.sdsCorner._t_name)
         .where(this.client.sdsCorner.code, id)
         .then(res => {
+          console.log(`RES=>`, res);
           if (res > 0) {
             return resolve(res[0][this.client.sdsCorner.CornerName]);
           } else {
