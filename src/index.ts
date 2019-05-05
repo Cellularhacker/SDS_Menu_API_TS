61e8de9a4fcf032e0fcfbf0dd4adfb0632874396
Jeffrey Lee <jeffrey@wizpace.com>
2019-05-06 00:03:11 +0900

    add package

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.listen(20001);
+app.all("/*", function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/?")
