6e05a32ab5ee8d36886af73fa231cb16f9d8aeb0
Jeffrey Lee <jeffrey@wizpace.com>
2019-05-06 03:14:47 +0900

    fixed mistyped

--- src/service/SDSCorner.ts
+++ src/service/SDSCorner.ts
@@ -3,7 +3,5 @@ import SDSCorner from "../model/maria/SDSCorner/SdsCorner";
 const sdsCorner = new SDSCorner();
 
 export default function getCornerName(id: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    return sdsCorner.ask(id);
-  });
+  return sdsCorner.ask(id);
 }
